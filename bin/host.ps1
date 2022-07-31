b0b30d14c84c2f5494e3fa229f473294684ca047
Alain Siegrist <alainsiegrist02@gmail.com>
2022-07-31 18:03:35 +0200

    Do not run game if build failed

--- bin/host.ps1
+++ bin/host.ps1
@@ -1,3 +1,4 @@
 cargo build
+if (-not $?) {throw "Failed to build"}
 start powershell {cargo run -- host; Write-Host "Exited"; Read-Host}
 start powershell {cargo run -- join 127.0.0.1:33998; Write-Host "Exited"; Read-Host}

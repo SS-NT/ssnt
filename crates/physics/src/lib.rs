3119f0021bee5d5796c15141afec21278f0ba3ab
Alain Siegrist <alainsiegrist02@gmail.com>
2023-07-17 21:22:06 +0200

    Funny hacks so physics works how I want

--- crates/physics/src/lib.rs
+++ crates/physics/src/lib.rs
@@ -6,8 +6,8 @@ use bevy::{
     prelude::{App, Plugin},
     reflect::Reflect,
 };
-use bevy_rapier3d::prelude::Collider as RapierCollider;
 use bevy_rapier3d::prelude::RigidBody as RapierRigidBody;
+use bevy_rapier3d::prelude::{Collider as RapierCollider, CollisionGroups, Group};
 use bevy_rapier3d::prelude::{ColliderDisabled, Real, RigidBodyDisabled};
 use serde::{Deserialize, Serialize};
 
@@ -21,6 +21,7 @@ impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<Collider>()
             .register_type::<ColliderType>()
+            .register_type::<ColliderGroup>()
             .register_type::<RigidBody>()
             .register_type::<RigidBodyType>()
             .register_type::<bevy_rapier3d::dynamics::ReadMassProperties>()
@@ -36,6 +37,7 @@ impl Plugin for PhysicsPlugin {
 #[reflect(Component)]
 struct Collider {
     kind: ColliderType,
+    group: ColliderGroup,
 }
 
 #[derive(Reflect, Serialize, Deserialize, Clone, Debug)]
@@ -55,6 +57,31 @@ impl Default for ColliderType {
     }
 }
 
+#[derive(Reflect, Serialize, Deserialize, Clone, Copy, Debug, Default)]
+#[reflect_value(Serialize, Deserialize)]
+pub enum ColliderGroup {
+    #[default]
+    Default,
+    CharacterColliders,
+    AttachedLimbs,
+}
+
+pub const DEFAULT_GROUP: Group = Group::GROUP_1;
+pub const LIMB_GROUP: Group = Group::GROUP_3;
+pub const RAYCASTING_GROUP: Group = Group::GROUP_32;
+
+impl From<ColliderGroup> for CollisionGroups {
+    fn from(value: ColliderGroup) -> Self {
+        match value {
+            ColliderGroup::Default => CollisionGroups::new(DEFAULT_GROUP, Group::ALL),
+            // Colliders on characters (pushing and blocking)
+            ColliderGroup::CharacterColliders => CollisionGroups::new(Group::GROUP_2, Group::ALL),
+            // Limbs attached to bodies collide with raycasts
+            ColliderGroup::AttachedLimbs => CollisionGroups::new(LIMB_GROUP, RAYCASTING_GROUP),
+        }
+    }
+}
+
 fn add_colliders(query: Query<(Entity, &Collider), Added<Collider>>, mut commands: Commands) {
     for (entity, loaded_collider) in query.iter() {
         let collider = match loaded_collider.kind {
@@ -64,7 +91,7 @@ fn add_colliders(query: Query<(Entity, &Collider), Added<Collider>>, mut command
         commands
             .entity(entity)
             .remove::<Collider>()
-            .insert(collider);
+            .insert((collider, CollisionGroups::from(loaded_collider.group)));
     }
 }
 
@@ -94,6 +121,8 @@ pub trait PhysicsEntityCommands {
     fn set_physics(&mut self, enabled: bool) -> &mut Self;
     fn enable_physics(&mut self) -> &mut Self;
     fn disable_physics(&mut self) -> &mut Self;
+    fn freeze(&mut self, new_group: Option<ColliderGroup>) -> &mut Self;
+    fn unfreeze(&mut self, new_group: Option<ColliderGroup>) -> &mut Self;
 }
 
 impl<'w, 's, 'a> PhysicsEntityCommands for EntityCommands<'w, 's, 'a> {
@@ -107,7 +136,34 @@ impl<'w, 's, 'a> PhysicsEntityCommands for EntityCommands<'w, 's, 'a> {
 
     fn set_physics(&mut self, enabled: bool) -> &mut Self {
         let entity = self.id();
-        self.commands().add(SetPhysicsCommand { entity, enabled });
+        self.commands().add(SetPhysicsCommand {
+            entity,
+            enabled,
+            disable_colliders: true,
+            new_group: None,
+        });
+        self
+    }
+
+    fn freeze(&mut self, new_group: Option<ColliderGroup>) -> &mut Self {
+        let entity = self.id();
+        self.commands().add(SetPhysicsCommand {
+            entity,
+            enabled: false,
+            disable_colliders: false,
+            new_group,
+        });
+        self
+    }
+
+    fn unfreeze(&mut self, new_group: Option<ColliderGroup>) -> &mut Self {
+        let entity = self.id();
+        self.commands().add(SetPhysicsCommand {
+            entity,
+            enabled: true,
+            disable_colliders: false,
+            new_group,
+        });
         self
     }
 }
@@ -119,21 +175,26 @@ struct TemporarilySensor;
 struct SetPhysicsCommand {
     entity: Entity,
     enabled: bool,
+    disable_colliders: bool,
+    new_group: Option<ColliderGroup>,
 }
 
 impl Command for SetPhysicsCommand {
     fn write(self, world: &mut World) {
         let mut root = world.entity_mut(self.entity);
 
-        // Freeze or unfreeze rigidbodies
         if self.enabled {
             root.remove::<RigidBodyDisabled>();
-        } else if !root.contains::<RigidBodyDisabled>() {
+        } else if root.contains::<RapierRigidBody>() && !root.contains::<RigidBodyDisabled>() {
             root.insert(RigidBodyDisabled);
         }
 
-        // Disable colliders
-        let mut to_change = Vec::new();
+        if !self.disable_colliders && self.new_group.is_none() {
+            return;
+        }
+
+        // Find colliders
+        let mut colliders = Vec::new();
         let mut children_system_state: SystemState<(Query<&Children>,)> = SystemState::new(world);
         let (child_query,) = children_system_state.get(world);
         for child_entity in child_query
@@ -141,18 +202,23 @@ impl Command for SetPhysicsCommand {
             .chain(std::iter::once(self.entity))
         {
             let child = world.entity(child_entity);
-            let is_enabled = !child.contains::<ColliderDisabled>();
-            if self.enabled != is_enabled {
-                to_change.push(child_entity);
+            if !child.contains::<RapierCollider>() {
+                continue;
             }
+            colliders.push(child_entity);
         }
 
-        for entity_id in to_change {
+        for entity_id in colliders {
             let mut entity = world.entity_mut(entity_id);
-            if self.enabled {
-                entity.remove::<ColliderDisabled>();
-            } else {
-                entity.insert(ColliderDisabled);
+            if self.disable_colliders {
+                if self.enabled {
+                    entity.remove::<ColliderDisabled>();
+                } else {
+                    entity.insert(ColliderDisabled);
+                }
+            }
+            if let Some(group) = self.new_group {
+                entity.insert(CollisionGroups::from(group));
             }
         }
     }

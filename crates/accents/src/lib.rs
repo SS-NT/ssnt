ce29100df432239f6bd4040b0b46e9173b6218a4
Eugene <fogaprod@gmail.com>
2023-11-06 16:42:19 +0400

    drop all deserialize logic

--- crates/accents/src/lib.rs
+++ crates/accents/src/lib.rs
@@ -1,48 +1,21 @@
 use serde::Deserialize;
 
-use pink_accents::deserialize::AccentDef as PinkAccentDef;
 use pink_accents::Accent as PinkAccent;
 
 #[derive(Debug, Deserialize)]
-struct AccentDef {
-    name: String,
-    description: String,
-    rules: PinkAccentDef,
-}
-
-#[derive(Debug)]
 pub struct Accent {
     pub name: String,
     pub description: String,
-    accent: PinkAccent,
-}
-
-impl TryFrom<AccentDef> for Accent {
-    type Error = String;
-
-    fn try_from(accent_def: AccentDef) -> Result<Self, Self::Error> {
-        Ok(Self {
-            name: accent_def.name,
-            description: accent_def.description,
-            accent: accent_def.rules.try_into()?,
-        })
-    }
+    rules: PinkAccent,
 }
 
 impl Accent {
     pub fn severities(&self) -> Vec<u64> {
-        self.accent.severities()
+        self.rules.severities()
     }
 
     pub fn apply(&self, text: &str, severity: u64) -> String {
-        self.accent.apply(text, severity)
-    }
-
-    pub fn from_ron(s: &str) -> Result<Self, String> {
-        Self::try_from(
-            ron::from_str::<AccentDef>(s)
-                .map_err(|err| format!("unable to load accent definition: {}", err))?,
-        )
+        self.rules.apply(text, severity)
     }
 }
 
@@ -58,7 +31,8 @@ mod tests {
             let filename = file.expect("getting file info").path();
             println!("parsing {}", filename.display());
 
-            let _ = Accent::from_ron(&fs::read_to_string(filename).expect("reading file")).unwrap();
+            let _ = ron::from_str::<Accent>(&fs::read_to_string(filename).expect("reading file"))
+                .expect("parsing ron definition");
         }
     }
 }
